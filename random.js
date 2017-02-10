f5843bded255cee5a5c801d09c66d2307afcc055
vincent <vicenteliuw@hotmail.com>
2017-02-10 11:26:49 -0800

    dncnzcdzn

--- random.js
+++ random.js
@@ -4,24 +4,54 @@ var namespace = "http://www.w3.org/2000/svg"
 // Fill in this function so that it draws something using SVG shapes!
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
-function createFirstScene() {
-    
+function createFirstScene() {  
+  makeRect(0,0,10000, 10000, "white", 1)
+   makeCircle(100,60,30,"orange",1)
+  makeRect(0,50,210,100,"blue",1)
+ makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",90,20,15,12,1)
+  makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",120,20,15,12,1)
+   makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",153,20,15,12,1)
+       makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",65,20,15,12,1)
+        makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",30,20,15,12,1)
+        makeImage("https://img.clipartfest.com/8bf0aa9fdb04cef0db66f09cf45d0006_monster-octopus-clipart-orange-octopus-clipart_675-502.png",25,60,35,32,1)
+        makeImage("http://clipartix.com/wp-content/uploads/2016/05/Salmon-fish-clip-art-free-clipart-images.png",140,60,30,24,1)
+        makePolygon(34,"black",1)
+        makeText("BOTTOM OF OCEAN", 10, 40, 5, "Pangolin", "blue", 1)
+        
 }
-
-
 // Fill in this function so that it draws something using SVG shapes!
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createSecondScene() {
-    
+    makeRect(0,0,1000,1000,"gray",1)
+   makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",90,90,20,12,1)
+    makeCircle(35,30,15,"orange",1)
+     makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",60,90,20,12,1)
+      makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",30,90,20,12,1)
+             makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",0,90,20,12,1)
+              makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",120,90,20,12,1)
+               makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",150,90,20,12,1)
+                makeImage("https://img.clipartfest.com/a7a3fc2f557335d673ad2cfd06d2f6a5_05aa795dabacdef12bf60bac5b98ca-free-clipart-images-cloud_8660-6920.svg",180,90,20,12,1)
+                makeImage("https://img.clipartfest.com/e25ae31a1ef950e193deb3fbda627b95_flying-paper-airplane-clipart-free-plane-clipart_1052-744.svg",120,50,50,22,1)
+                makeImage("http://inventors.phillipmartin.info/corsair.png",40,60,60,32,1)
+                makePolygon(34,"black",1)
+                 makeText("BOTTOM OF OCEAN", 10, 40, 5, "Pangolin", "blue", 1)
+                
 }
+
 
 
 // Fill in this function so that it draws something using SVG shapes!
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createThirdScene() {
-    
+    makeRect(0,0,1000,1000,"black",1)
+     makeCircle(35,30,15,"orange",1)
+     makeImage("http://carpng.com/wp-content/uploads/thumb/space-shuttle-8668-0.png",40,60,60,32,1)
+     makeImage("http://donnamanobianco.com/uploads/3/5/8/0/3580689/2650618_orig.png",100,40,60,32,1)
+     makeImage("http://frpic.com/vectors/moon-clipart/moon-clipart-3.png",150,10,60,32,1)
+     makeRect(1131,1223,12,12,"black",1)
+     makePolygon(2590,"black",1)
 }
 
 
@@ -31,6 +61,14 @@ function createThirdScene() {
 // FILL IN THIS FUNCTION!
 // This function is called whenever you press the "Go!" button.
 function createRandomScene() {
+    varmyRandom = Math.random()
+if(varmyRandom<0.1){
+    createFirstScene()
+}else if(varmyRandom<0.67){
+    createSecondScene()
+}else{
+    createThirdScene()
+}
     // Generate a random number between 0 and 1, and store it in a variable.
     
     // If the number is less than 0.33, call the function to create your first scene.
